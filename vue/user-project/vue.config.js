bbbd05f0aad2b51812e09ef510993d3408bf707c
yb1012 <tgu02007@naver.com>
2023-12-01 17:46:55 +0900

    12.01

--- vue/user-project/vue.config.js
+++ vue/user-project/vue.config.js
@@ -8,7 +8,8 @@ module.exports = defineConfig({
     proxy : {//어떤경우에 origin을 대체할건지 써주면 됨
       '^/':{ //이경로로 시작하는 오리진이라는뜻
         target,
-        changeOrigin : true //얘가 트루여야 위경로로 시작하는 오리진을 바꿔줌
+        changeOrigin : true, //얘가 트루여야 위경로로 시작하는 오리진을 바꿔줌
+        ws:false
       }
     }
   }
